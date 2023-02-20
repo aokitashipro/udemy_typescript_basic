a7ff11fb4034bfcc3061d3e8cef4ebc705881626
aokitashipro <webmocha2@gmail.com>
2023-02-20 19:08:37 +0900

    add async

---
+++ section3/dist/event.js
@@ -0,0 +1,13 @@
+// const target = document.getElementById('username')
+//     target.addEventListener('input', (e) => {
+//       console.log(e)
+//       if(e.target instanceof HTMLInputElement){
+//         console.log(e.target.value)
+//       } else {
+//       }
+//     })
+const target = document.getElementById('username');
+target.addEventListener('input', (e) => {
+    console.log(e.target.value);
+});
+//# sourceMappingURL=event.js.map
